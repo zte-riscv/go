19a0c932b5ef3ed68c60ca2c91f7f22496a49c03
Qiuweihong <953950914@qq.com>
2025-09-05 11:08:42 +0800

    test asm

--- test/zte/vlseg_riscv64.s
+++ test/zte/vlseg_riscv64.s
@@ -18,20 +18,15 @@ TEXT ·vlseg2Deinterleave(SB), NOSPLIT, $0-72
 	// nPairs = in_len / 2
 	SRLI	$1, X11, X11
 
-    MOVBU (X10), X5
-    MOVB X5, (X13)
-
 	BEQZ	X11, done
-
-    MOVB X5, (X16)
 
 loop:
 	// 设置 vl = min(nPairs, VLEN/SEW)，SEW=E8, LMUL=M1，TA/MA
 	// rd=X12 接收实际 vl，rs1=X11 提供 nPairs（使用调用者保存寄存器）
-	VSETVLI	X12, E8, M1, TA, MA, X11
+	VSETVLI	X12, X11, E8, M1, TA, MA
 
 	// 分段加载：v8=偶数元素，v9=奇数元素（以对为单位）
-	VLSEG2E8V	(X10), V8
+	VLSEG2E8V	V8, (X10)
 
 	// 分别存回两个输出缓冲
 	VSE8V	V8, (X13)
