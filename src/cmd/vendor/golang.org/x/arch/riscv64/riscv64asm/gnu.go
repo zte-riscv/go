5780953b1a7c25c6fb4938aecda23038f9027a74
wenchangping <987706386@qq.com>
2025-11-04 20:22:04 +0800

    cmd/vendor/golang.org/x/arch: add disasm support for the Zicbop extension of riscv64 (#58)
    * cmd/vendor/golang.org/x/arch: add disasm support for the Zicbop extension of riscv64
    
    ---------
    
    Co-authored-by: 文昌平10295505 <wen.changping@zte.com.cn>

--- src/cmd/vendor/golang.org/x/arch/riscv64/riscv64asm/gnu.go
+++ src/cmd/vendor/golang.org/x/arch/riscv64/riscv64asm/gnu.go
@@ -58,6 +58,24 @@ func GNUSyntax(inst Inst) string {
 			args = args[:len(args)-1]
 		}
 
+		if inst.Op == ORI && inst.Args[0].(Reg) == X0 {
+			imm := inst.Args[2].(Simm).Imm
+			switch imm & ((1 << 5) - 1) {
+			case 0:
+				op = "prefetch.i"
+			case 1:
+				op = "prefetch.r"
+			case 3:
+				op = "prefetch.w"
+			}
+			if imm == 0 {
+				args[0] = fmt.Sprintf("(X%d)", inst.Args[1].(Reg))
+			} else {
+				args[0] = fmt.Sprintf("%s(X%d)", inst.Args[2].(Simm).String(), inst.Args[1].(Reg))
+			}
+			args = args[:len(args)-2]
+		}
+
 	case ADD:
 		if inst.Args[1].(Reg) == X0 {
 			if inst.Args[0].(Reg) == X0 {
