c20301fc10bbbf867717496eb1412457477bdda9
Qiuweihong <953950914@qq.com>
2025-11-17 10:31:28 +0800

    restore extractBitAndShift because zimop may use it

--- src/cmd/internal/obj/riscv/obj.go
+++ src/cmd/internal/obj/riscv/obj.go
@@ -1641,6 +1641,12 @@ func validateRaw(ctxt *obj.Link, ins *instruction) {
 	}
 }
 
+// extractBitAndShift extracts the specified bit from the given immediate,
+// before shifting it to the requested position and returning it.
+func extractBitAndShift(imm uint32, bit, pos int) uint32 {
+	return ((imm >> bit) & 1) << pos
+}
+
 // encodeBitPattern encodes an immediate value by extracting the specified
 // bit pattern from the given immediate. Each value in the pattern specifies
 // the position of the bit to extract from the immediate, which are then
