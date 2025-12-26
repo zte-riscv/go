8d7c73c4953390169aeb60643f98e31300938a16
cuitiankai10342768 <cui.tiankai@zte.com.cn>
2025-12-26 16:11:40 +0800

    fix bug and remove assemblies

--- src/cmd/compile/internal/ssagen/intrinsics_test.go
+++ src/cmd/compile/internal/ssagen/intrinsics_test.go
@@ -1122,7 +1122,6 @@ var wantIntrinsics = map[testIntrinsicKey]struct{}{
 	{"riscv64", "internal/runtime/atomic", "Xaddint64"}:                struct{}{},
 	{"riscv64", "internal/runtime/atomic", "Xadduintptr"}:              struct{}{},
 	{"riscv64", "internal/runtime/atomic", "Xchg"}:                     struct{}{},
-	{"riscv64", "internal/runtime/atomic", "Xchg8"}:                    struct{}{},
 	{"riscv64", "internal/runtime/atomic", "Xchg64"}:                   struct{}{},
 	{"riscv64", "internal/runtime/atomic", "Xchgint32"}:                struct{}{},
 	{"riscv64", "internal/runtime/atomic", "Xchgint64"}:                struct{}{},
