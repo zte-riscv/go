40ada752775b5cc77ff93c83742cb31c171b58c0
Qiuweihong <953950914@qq.com>
2025-12-29 15:45:51 +0800

    revert the ABI change

--- src/runtime/asm_riscv64.s
+++ src/runtime/asm_riscv64.s
@@ -270,9 +270,9 @@ TEXT gogo<>(SB), NOSPLIT|NOFRAME, $0
 	JALR	ZERO, T0
 
 // func procyield(cycles uint32)
-TEXT runtime·procyield<ABIInternal>(SB),NOSPLIT,$0-0
-#ifdef EnableRuntimeSpinlock
-	MOVW	X10, T0
+TEXT runtime·procyield(SB),NOSPLIT,$0-0
+#ifdef EnableRISCV64RuntimeSpinlock
+	MOVWU	cycles+0(FP), T0
 yieldloop:
 	PAUSE
 	SUBW	$1, T0
