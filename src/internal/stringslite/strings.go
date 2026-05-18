ecb39b0592df4363b92e9e35454f6eafcadb1f66
Qiuweihong <953950914@qq.com>
2026-05-18 16:03:19 +0800

    fix bugs

--- src/internal/stringslite/strings.go
+++ src/internal/stringslite/strings.go
@@ -39,7 +39,7 @@ func Index(s, substr string) int {
 		return -1
 	case n > len(s):
 		return -1
-	case n <= bytealg.MaxLen && bytealg.UseFastIndex():
+	case n <= bytealg.MaxLen:
 		// Use brute force when s and substr both are small
 		if len(s) <= bytealg.MaxBruteForce {
 			return bytealg.IndexString(s, substr)
