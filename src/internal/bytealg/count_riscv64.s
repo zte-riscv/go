2bf19ab12ebb1955f4cfceaa28913d92e8c1e3bc
Xueqi Luo <79146446+lxq015@users.noreply.github.com>
2026-06-12 14:35:48 +0800

    internal/bytealg: vector implementation of count 1 byte for riscv64 (#123)
    https://go-review.googlesource.com/c/go/+/657036
    This CL provide a vector implementation of count 1 byte
    
    goos: linux
    goarch: riscv64
    pkg: bytes
    cpu: Spacemit(R) X60
                    │         sec/op         │   sec/op     vs base                │
    CountSingle/10               42.12n ± 0%   44.49n ± 0%   +5.64% (p=0.000 n=10)
    CountSingle/32               85.74n ± 0%   44.49n ± 0%  -48.11% (p=0.000 n=10)
    CountSingle/4K              8371.5n ± 0%   396.7n ± 0%  -95.26% (p=0.000 n=10)
    CountSingle/4M              8929.1µ ± 0%   756.9µ ± 0%  -91.52% (p=0.000 n=10)
    CountSingle/64M             159.78m ± 0%   13.89m ± 0%  -91.30% (p=0.000 n=10)
    geomean                      33.65µ        6.073µ       -81.95%
    
                    │ /root/scalar_count.log │         /root/vector_count.log          │
                    │          B/s           │      B/s       vs base                  │
    CountSingle/10              226.4Mi ± 0%    214.3Mi ± 0%     -5.34% (p=0.000 n=10)
    CountSingle/32              355.9Mi ± 0%    685.9Mi ± 0%    +92.71% (p=0.000 n=10)
    CountSingle/4K              466.6Mi ± 0%   9846.5Mi ± 0%  +2010.19% (p=0.000 n=10)
    CountSingle/4M              448.0Mi ± 0%   5284.9Mi ± 0%  +1079.74% (p=0.000 n=10)
    CountSingle/64M             400.5Mi ± 0%   4606.2Mi ± 0%  +1049.98% (p=0.000 n=10)
    geomean                     368.0Mi         1.991Gi        +454.08%

--- src/internal/bytealg/count_riscv64.s
+++ src/internal/bytealg/count_riscv64.s
@@ -2,34 +2,54 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
+#include "asm_riscv64.h"
 #include "go_asm.h"
 #include "textflag.h"
 
-TEXT ·CountString<ABIInternal>(SB),NOSPLIT,$0-32
+TEXT ·CountString<ABIInternal>(SB),NOSPLIT|NOFRAME,$0
 	// X10 = s_base
 	// X11 = s_len
 	// X12 = byte to count
-	MOV	X12, X13
+	MOV	X12, X13 // byte to Count wanted in X13
 	JMP	·Count<ABIInternal>(SB)
 
-TEXT ·Count<ABIInternal>(SB),NOSPLIT,$0-40
+TEXT ·Count<ABIInternal>(SB),NOSPLIT|NOFRAME,$0
 	// X10 = b_base
 	// X11 = b_len
 	// X12 = b_cap (unused)
-	// X13 = byte to count (want in X12)
-	AND	$0xff, X13, X12
-	MOV	ZERO, X14	// count
-	ADD	X10, X11	// end
+	// X13 = byte to count
+	MOV	X10, X14	// src pointer
+	MOV	ZERO, X10	// reset counter
+	AND	$0xff, X13	// make sure it's a byte to compare
+	SUB	$8, X11, X5
+	BLEZ	X5, count_scalar
+#ifndef hasV
+	MOVB	internal∕cpu·RISCV64+const_offsetRISCV64HasV(SB), X5
+	BEQZ	X5, count_scalar
+#endif
+	PCALIGN	$16
+count_vector_loop:
+	VSETVLI	X11, E8, M8, TA, MA, X5
+	VLE8V	(X14), V8
+	VMSEQVX	X13, V8, V1
+	VCPOPM	V1, X15
+	ADD	X15, X10	// add counter
+	ADD	X5, X14
+	SUB	X5, X11
+	BEQZ	X11, done
+	JMP	count_vector_loop
 
 	PCALIGN	$16
-loop:
-	BEQ	X10, X11, done
-	MOVBU	(X10), X15
-	ADD	$1, X10
-	BNE	X12, X15, loop
+count_scalar:
+	ADD	X14, X11	// end pointer
+	PCALIGN	$16
+count_scalar_loop:
+	BEQ	X14, X11, done
+	MOVBU	(X14), X15
 	ADD	$1, X14
-	JMP	loop
+	BNE	X13, X15, count_scalar_loop
+	ADD	$1, X10
+	JMP	count_scalar_loop
 
 done:
-	MOV	X14, X10
 	RET
