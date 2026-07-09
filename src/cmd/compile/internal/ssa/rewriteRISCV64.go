dc40835f3bef7ee5fdc0a825d15ebc2c5da59156
文昌平10295505 <wen.changping@zte.com.cn>
2026-07-09 11:08:47 +0800

    cmd/compile/internal/ssa: optimize float zero stores to use integer storezero ops on riscv64

--- src/cmd/compile/internal/ssa/rewriteRISCV64.go
+++ src/cmd/compile/internal/ssa/rewriteRISCV64.go
@@ -4192,6 +4192,27 @@ func rewriteValueRISCV64_OpRISCV64FMOVDstore(v *Value) bool {
 		v.AddArg3(base, val, mem)
 		return true
 	}
+	// match: (FMOVDstore [off] {sym} ptr val:(FMOVDconst) mem)
+	// cond: val.AuxInt == 0
+	// result: (MOVDstorezero [off] {sym} ptr mem)
+	for {
+		off := auxIntToInt32(v.AuxInt)
+		sym := auxToSym(v.Aux)
+		ptr := v_0
+		val := v_1
+		if val.Op != OpRISCV64FMOVDconst {
+			break
+		}
+		mem := v_2
+		if !(val.AuxInt == 0) {
+			break
+		}
+		v.reset(OpRISCV64MOVDstorezero)
+		v.AuxInt = int32ToAuxInt(off)
+		v.Aux = symToAux(sym)
+		v.AddArg2(ptr, mem)
+		return true
+	}
 	return false
 }
 func rewriteValueRISCV64_OpRISCV64FMOVWload(v *Value) bool {
@@ -4312,6 +4333,27 @@ func rewriteValueRISCV64_OpRISCV64FMOVWstore(v *Value) bool {
 		v.AuxInt = int32ToAuxInt(off1 + int32(off2))
 		v.Aux = symToAux(sym)
 		v.AddArg3(base, val, mem)
+		return true
+	}
+	// match: (FMOVWstore [off] {sym} ptr val:(FMOVFconst) mem)
+	// cond: int32(val.AuxInt) == 0
+	// result: (MOVWstorezero [off] {sym} ptr mem)
+	for {
+		off := auxIntToInt32(v.AuxInt)
+		sym := auxToSym(v.Aux)
+		ptr := v_0
+		val := v_1
+		if val.Op != OpRISCV64FMOVFconst {
+			break
+		}
+		mem := v_2
+		if !(int32(val.AuxInt) == 0) {
+			break
+		}
+		v.reset(OpRISCV64MOVWstorezero)
+		v.AuxInt = int32ToAuxInt(off)
+		v.Aux = symToAux(sym)
+		v.AddArg2(ptr, mem)
 		return true
 	}
 	return false
